3f5cafb8af3b1db55f4f8ad0a9df2069a321c971
Jon Morgan <jonmorg@iinet.net.au>
2021-09-07 18:31:10 +1000

    updates

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # homework_wk3
 PASSWORD GENERATOR
+
+Homework Week 3 required us to modify the supplied starter code to create an application that enabled employees to generate random passwords based on criteria that they’ve selected. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code.
+
+<img src="assets/images/screenshot.png" >
+
+The meet the homework requirements of selecting criteria to include in the password generator to create a password which includes based on user input upper and lower case, numeric and special characters with a length of between 8 and 128 characters, I added slider and checkbox elements.
--- script.js
+++ script.js
@@ -12,3 +12,34 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+
+  var number = document.getElementById("myRange").value;
+  var charlength = document.getElementById("charlength");
+  charlength.innerHTML = number;
+  var length = number,
+      charset = "abcdefghijklmnopqrstuvwxyz",
+      retVal = "";
+
+      var uppercase = document.getElementById("uppercase");   
+      if (uppercase.checked == true){
+      charset += uppercase.value;
+      }
+
+      var numeric = document.getElementById("numeric");   
+      if (numeric.checked == true){
+      charset += numeric.value;
+      }  
+ 
+      var special = document.getElementById("special");   
+      if (special.checked == true){
+      charset += special.value;
+      } 
+
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+
